08c7dc0aec82a1a263011eaba1f7c671e405bfb2
notseekeru <myteseeker18@gmail.com>
2026-04-09 09:22:08 +0800

    chore: testing fail ci commit again

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -2,9 +2,9 @@ package controller;
 
 import java.io.IOException;
 
-Code breaking commit: 9c8e5b1a2f0c4e5b6d7e8f9a0b1c2d3e4f5g6h7import javafx.fxml.FXML;
-
 import javafx.scene.control.PasswordField;
+
+Code breaking commit: 9c8e5b1a2f0c4e5b6d7e8f9a0b1c2d3e4f5g6h7import javafx.fxml.FXML;
 
 import javafx.scene.control.TextField;
 
