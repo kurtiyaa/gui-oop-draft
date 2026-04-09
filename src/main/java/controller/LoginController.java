b3fa1512a8ca8c693a4eb77f5236e923780dfd00
notseekeru <myteseeker18@gmail.com>
2026-04-09 09:23:33 +0800

    chore: remove commented code from LoginController and finalized testing

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 
 import javafx.scene.control.PasswordField;
 
-Code breaking commit: 9c8e5b1a2f0c4e5b6d7e8f9a0b1c2d3e4f5g6h7import javafx.fxml.FXML;
-
 import javafx.scene.control.TextField;
 
 public class LoginController {
